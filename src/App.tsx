f082b5aa74ceb078318b9ec278fd698a14f652f3
PavelNovik <92117738+PavelNovik@users.noreply.github.com>
2023-08-21 23:03:23 +0300

    Create all elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import background from './images/pic1.jpg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Box>
+                <ImgContainer/>
+                <Title>Headline</Title>
+                <Article>Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.</Article>
+                <ActionBtn>See more</ActionBtn>
+                <ActionBtn>Save</ActionBtn>
+
+            </Box>
+        </div>
+    );
 }
 
 export default App;
+
+const Box = styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@500;700&display=swap');
+  width: 300px;
+  height: 350px;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-radius: 15px;
+  box-shadow: 0px 4px 20px 5px rgba(0, 0, 0, 0.10);
+`
+const ImgContainer = styled.div`
+  width: 280px;
+  height: 170px;
+  border-radius: 10px;
+  background: center / cover no-repeat url(${background}), lightgray 50%;
+
+`
+const Title = styled.h1`
+  color: #000;
+  font-family: 'Inter', sans-serif;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`
+const Article = styled.p`
+  color: #ABB3BA;
+  font-family: 'Inter', sans-serif;
+  font-size: 0.75rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 1.25rem;
+`
+const ActionBtn = styled.button`
+  width: 5.375rem;
+  height: 1.875rem;
+  border-radius: 0.3125rem;
+  background: #4E71FE;
+`
---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,18 @@
+import {createGlobalStyle} from "styled-components";
+
+export const GlobalStyles = createGlobalStyle `
+  *,
+  *::before,
+  *::after {    
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;    
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: #c4c3c3;
+  }
+`
